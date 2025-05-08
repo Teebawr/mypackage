ff30c267988dec72b566897148c93e17ca649ff6
Teebawr <tudor.olariu@empa.ch>
2025-05-08 09:51:44 +0000

    Added yaml file with pre-commit and ruff

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,5 +1,9 @@
 from .geometry import Point, Line
 from .utils import distance
 
+
 def personal_message():
     print("Hello from John Doe!")
+
+
+__all__ = ["Point", "Line", "distance"]
--- mypackage/geometry/__init__.py
+++ mypackage/geometry/__init__.py
@@ -1,1 +1,3 @@
-from .geometry import Point, Line
+from .geometry import Point, Line, distance
+
+__all__ = ["Point", "Line", "distance"]
